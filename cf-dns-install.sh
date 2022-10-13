b1776b25673297493a4e829f10402f7a38a62af2
Josh S <joshsevero208@gmail.com>
2022-10-13 10:00:03 -0400

    Update cf-dns-install.sh

--- cf-dns-install.sh
+++ cf-dns-install.sh
@@ -1,5 +1,12 @@
 sudo apt install -y jq
 wget https://raw.githubusercontent.com/DIVISIONSolar/cloudflare-dns-update/main/scripts/cf-dns-update.sh
 mv cf-dns-update.sh cloudflare-dns
-echo "* * * * * ./cloudflare-dns/cf-dns-update.sh" > crontab -e 
+echo "Files downloaded"
+echo "
+run nano ./cloudflare-dns/cf-dns-update.sh to finish setup
+and
+run: crontab -e and fill it with:
+* * * * * ./cloudflare-dns/cf-dns-update.sh
+"
+
 echo "Files downloaded, run nano ./cloudflare-dns/cf-dns-update.sh to finish setup"
